b02c477368dd9998cc0c71d8c754d83f3211aa49
jairjunior28 <jairjunior242011@gmail.com>
2024-04-02 20:06:50 -0300

    aula 1 finalizada, com diferença que nossa aplicação está utilizando banco de dados mysql, não hypersql como no NLW mas nada muda em códigos, foi somente nescessário adicionar alguns pacotes e configuração em properties com relação a conexão com banco de dados;

--- src/main/java/jrsoftwork/com/pass/in/controllers/AttendeerController.java
+++ src/main/java/jrsoftwork/com/pass/in/controllers/AttendeerController.java
@@ -1,9 +1,15 @@
 package jrsoftwork.com.pass.in.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/attendeers")
 public class AttendeerController {
+    @GetMapping
+    public ResponseEntity<String> getTeste(){
+        return ResponseEntity.ok("success");
+    }
 }
