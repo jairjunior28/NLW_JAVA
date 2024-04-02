fd8fe88d39de7719aa7bf149f7b427fa54e94701
jairjunior28 <jairjunior242011@gmail.com>
2024-04-02 19:53:30 -0300

    alterações de localizações camada domain

--- src/main/java/jrsoftwork/com/pass/in/controllers/CheckinController.java
+++
@@ -1,9 +0,0 @@
-package jrsoftwork.com.pass.in.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/checkin")
-public class CheckinController {
-}
