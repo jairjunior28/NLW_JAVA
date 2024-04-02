0df8721e5bcf74d1395621cfbd7a3d1712b964b8
jairjunior28 <jairjunior242011@gmail.com>
2024-04-02 00:21:13 -0300

    initial configuration of database

---
+++ src/test/java/jrsoftwork/com/pass/in/ApplicationTests.java
@@ -0,0 +1,13 @@
+package jrsoftwork.com.pass.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
